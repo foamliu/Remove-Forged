b9b9ccf4b6da1c1209ad645cee9c3b488bb11ab6
Yang Liu <liuyang12@focusmedia.cn>
2019-06-26 14:08:18 +0800

    update

--- remove_forged.py
+++ remove_forged.py
@@ -4,11 +4,11 @@ with open('prefix.txt', 'r') as file:
 prefix_set = {x.strip() for x in lines}
 
 
-def is_forged(mac):
+def is_real(mac):
     check = mac[0:6].upper()
     return check in prefix_set
 
 
 if __name__ == '__main__':
-    print(is_forged('482CA04A7D89'))
-    print(is_forged('000000000000'))
+    print(is_real('482CA04A7D89'))
+    print(is_real('000000000000'))
